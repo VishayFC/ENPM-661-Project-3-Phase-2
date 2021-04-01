f37312a415861f171e49bf2c079f2ed077823b51
ADITYA JADHAV <amjadhav@umd.edu>
2021-04-01 00:30:23 -0400

    Creating a Queue Class

---
+++ Project3_Phase2.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import math
+
+out = cv2.VideoWriter('Exploration.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 60, (400,300))
+
+#defining the queue class to use as a data structure
+class queue():
+    def __init__(self):
+        self.pending = list()
+
+    def add(self, child, ind):
+        self.pending.insert(ind, child)
+
+    def remove(self):
+        if self.pending:
+            return self.pending.pop()
+        return None
+
+    def peek(self):
+        if self.pending:
+            return self.pending[-1]
+
+    def size1(self):
+        return len(self.pending)
+
+    def isempty(self):
+        if self.pending == []:
+            return True
+        return False
